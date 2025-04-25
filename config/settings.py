08c36ee897b575713bb1518e286fe61a6fbcc135
SHOXAKONG <bekmurodovshohruh0224@egmail.com>
2025-04-26 00:39:02 +0500

    Second Commit

--- config/settings.py
+++ config/settings.py
@@ -212,5 +212,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "bekmurodovshohruh0224@gmail.com"
-EMAIL_HOST_PASSWORD = "hexlwixfyybczxea"
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
