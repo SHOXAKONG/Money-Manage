7aadf2064b53b2988361726c08b6761278606c4d
SHOXAKONG <bekmurodovshohruh0224@egmail.com>
2025-04-26 23:07:14 +0500

    Initial Commit

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["python3linux.pythonanywhere.com, cash-flow-manager.netlify.app"]
 
 # Application definition
 
