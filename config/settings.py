a2309ae7871482a5a8c93bf1435a6400d7a52dd0
SHOXAKONG <bekmurodovshohruh0224@egmail.com>
2025-04-28 10:10:20 +0500

    Initial Commit

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -200,10 +200,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
-FRONTEND_URL = "http://localhost:5500/"
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = "bekmurodovshohruh0224@gmail.com"
-# EMAIL_HOST_PASSWORD = "hexl wixf yybc zxea"
+FRONTEND_URL = "https://cash-flow-manager.netlify.app/"
